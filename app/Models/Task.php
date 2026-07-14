419ce50d278ca3cbdf11ff3657f7efc6c4e14aed
Reslimyuu <120614167+Reslimyuu@users.noreply.github.com>
2026-07-14 20:29:26 +0800

    Install Laravel Breeze Authetification

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'name',
+        'status',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+
+    }
 }
